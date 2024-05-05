cf4b3f18dafda7ad47ff9d3039a5f85fc49c2b5a
Dibendu Ghosal <dibendughosal@gmail.com>
2024-05-05 23:11:57 +0530

    Day 7

---
+++ Practice Question/pattern7.c
@@ -0,0 +1,27 @@
+/* 
+find the given pattern -
+                1
+                1 3
+                1 3 5
+                1 3 5 7
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of Rows: "); //4
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        int a = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ",a);
+            a+=2;
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
