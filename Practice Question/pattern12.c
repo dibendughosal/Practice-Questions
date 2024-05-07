d553d3b8dd4ae96077a2cc54e796e0eb9f1044aa
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2024-05-07 12:35:44 +0530

    Day 9

---
+++ Practice Question/pattern12.c
@@ -0,0 +1,28 @@
+/* 
+    print the given pattern
+    * * * * *
+    *       *
+    *       *
+    * * * * *
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the Number of rows: ");
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j= 1; j <= n; j++)
+        {
+            if (i==1 || i==n || j==1 || j==n) {
+                printf("*");   
+            } 
+            else printf(" ");
+        }
+        
+        printf("\n");
+        
+    }
+    
+}
